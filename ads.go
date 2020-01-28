6c451041256d61f5ec9805f172ff83aefab5c24b
Sergey <serg3091@gmail.com>
2020-01-28 21:41:13 +0300

    add TargetGroup

--- ads.go
+++ ads.go
@@ -2,18 +2,47 @@ package vkapi
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/url"
 	"strconv"
 )
 
-type TargetGroupResponse struct {
+type CreateTargetGroupResponse struct {
 	ID int `json:"id"`
 }
 
-func (client *VKClient) CreateTargetGroup(AccountID string, lifetime string, name string) (TargetGroupResponse, error) {
-	fmt.Println("CreateTargetGroup....")
-	d := TargetGroupResponse{}
+// id (integer) — идентификатор аудитории;
+// name (string) — название аудитории ретаргетинга;
+// last_updated (integer) — дата и время последнего пополнения аудитории в формате Unixtime;
+// is_audience (integer, 1) — 1, если группа является аудиторией (т.е.может быть пополнена при помощи пикселя);
+// is_shared (integer, 1) — 1, если группа является копией (см. метод ads.shareTargetGroup);
+// audience_count (integer) — приблизительное количество пользователей, входящих в аудиторию;
+// lifetime (integer) — количество дней, через которое пользователи, добавляемые в аудиторию ретаргетинга, будут автоматически исключены из неё;
+// file_source (integer, 1) — признак пополнения аудитории через файл;
+// api_source (integer, 1) — признак пополнения аудитории через метод ads.importTargetContacts;
+// lookalike_source (integer, 1) — признак аудитории, полученной при помощи Look-a-Like.
+//
+// pixel (string) — код для размещения на сайте рекламодателя. Возвращается, если параметр extended = 1 (только для старых групп).
+// domain (string) — домен сайта, где размещен код учета пользователей (только для старых групп).
+type TargetGroupResponse struct {
+	ID int `json:"id"`
+
+	Name            string `json:"name"`
+	LastUpdated     int    `json:"last_updated"`
+	IsAudience      bool   `json:"is_audience"`
+	IsShared        bool   `json:"is_shared"`
+	AudienceCount   int    `json:"audience_count"`
+	Lifetime        int    `json:"lifetime"`
+	FileSource      bool   `json:"file_source"`
+	ApiSource       bool   `json:"api_source"`
+	LookalikeSource bool   `json:"lookalike_source"`
+	Pixel           string `json:"pixel"`
+	Domain          string `json:"domain"`
+}
+
+//
+func (client *VKClient) CreateTargetGroup(AccountID string, lifetime string, name string) (CreateTargetGroupResponse, error) {
+	d := CreateTargetGroupResponse{}
 	v := url.Values{}
 	v.Add("account_id", AccountID)
 	v.Add("lifetime", lifetime)
@@ -26,19 +55,56 @@ func (client *VKClient) CreateTargetGroup(AccountID string, lifetime string, nam
 
 	err = json.Unmarshal(resp.Response, &d)
 	if err != nil {
+		log.Println(err, string(resp.Response))
 		return d, err
 	}
 
 	return d, nil
 }
 
-//func (client *VKClient) DeleteTargetGroup(AccountID string, TargetGroupID string) error {
-//	v := url.Values{}
-//	v.Add("account_id", AccountID)
-//	v.Add("lifetime", TargetGroupID)
-//	client.MakeRequest("ads.deleteTargetGroup", v)
-//	return nil
-//}
+func (client *VKClient) GetTargetGroup(AccountID string) ([]TargetGroupResponse, error) {
+	d := []TargetGroupResponse{}
+	v := url.Values{}
+	v.Add("account_id", AccountID)
+
+	resp, err := client.MakeRequest("ads.getTargetGroups", v)
+	if err != nil {
+		return d, err
+	}
+
+	err = json.Unmarshal(resp.Response, &d)
+	if err != nil {
+		log.Println(err, string(resp.Response))
+		return d, err
+	}
+
+	return d, nil
+}
+
+func (client *VKClient) UpdateTargetGroup(AccountID string, TargetGroupID int) (int, error) {
+	d := 0
+	return d, nil
+}
+
+//
+func (client *VKClient) DeleteTargetGroup(AccountID string, TargetGroupID int) (int, error) {
+	d := 0
+	v := url.Values{}
+	v.Add("account_id", AccountID)
+	v.Add("target_group_id", strconv.Itoa(TargetGroupID))
+	resp, err := client.MakeRequest("ads.deleteTargetGroup", v)
+
+	if err != nil {
+		return d, err
+	}
+
+	err = json.Unmarshal(resp.Response, &d)
+	if err != nil {
+		return d, err
+	}
+
+	return d, nil
+}
 
 // Возвращает количество обработанных контактов.
 type ImportTargetContactsResponse struct {
@@ -68,8 +134,4 @@ func (client *VKClient) ImportTargetContacts(
 	}
 
 	return d, nil
-}
-
-func parseContacts() string {
-	return ""
 }
